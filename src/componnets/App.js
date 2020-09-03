85957181ef527bf29458cd9061d0a831688e0dd1
Alan <alansal_11@hotmail.com>
2020-09-02 23:23:18 -0500

    New routes were created

--- src/componnets/App.js
+++ src/componnets/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import LinkList from './Link/List'
-import CreateLink from './Link/Create';
+import CreateLink from './Link/Create'
 import Header from './Layout/Header'
+import Login from './Login/Login'
+import Search from './Search/Search'
 
 class App extends Component {
     render() {
@@ -12,8 +14,12 @@ class App extends Component {
                 <Header />
                 <div className="ph3 pv1 background-gray">
                     <Switch>
-                        <Route exact path="/" component={LinkList} />
+                        <Route exact path='/' render={() => <Redirect to='/new/1' />} />
                         <Route exact path="/create" component={CreateLink} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path='/search' component={Search} />
+                        <Route exact path='/top' component={LinkList} />
+                        <Route exact path='/new/:page' component={LinkList} />
                     </Switch>
                 </div>
             </div>
